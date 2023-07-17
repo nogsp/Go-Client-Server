9bac48643040e9f00a7d9f10be8f95fedc66f85f
Luís Alves <lema@cin.ufpe.br>
2023-07-17 01:53:55 -0300

    fix TCP -> one connection

--- server/serverTCP.go
+++ server/serverTCP.go
@@ -15,40 +15,57 @@ const (
 )
 
 func main() {
-	listen, err := net.Listen(TYPE, HOST+":"+PORT)
+
+	r, err := net.ResolveTCPAddr(TYPE, HOST+":"+PORT)
 	if err != nil {
 		log.Fatal(err)
 		os.Exit(1)
 	}
+
+	listen, err:= net.ListenTCP("tcp", r)
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+
 	defer listen.Close()
 
 	println("Servidor pronto para receber mensagens TCP.")
+
+
 	for {
 		conn, err := listen.Accept()
 		if err != nil {
 			log.Fatal(err)
 			os.Exit(1)
 		}
+
 		go handleRequest(conn)
 	}
-
 }
 
 func handleRequest(conn net.Conn) {
 	// incoming request
 	buffer := make([]byte, 1024)
-	bytesRead, err := conn.Read(buffer)
-	if err != nil {
-		log.Fatal(err)
+
+	for {
+		bytesRead, err := conn.Read(buffer)
+		if err != nil {
+			println(err)
+			break // Exit the loop when encountering an error
+		}
+		n, err := strconv.Atoi(string(buffer[:bytesRead]))
+
+		//fmt.Printf("Recebido de %s: %d\n", conn.LocalAddr().String(), n)
+		// write data to response
+		_, err = conn.Write([]byte(strconv.Itoa(fibo(n))))
+		if err != nil {
+			println(err)
+			break // Exit the loop when encountering an error
+		}
 	}
-	n, err := strconv.Atoi(string(buffer[:bytesRead]))
 
-	//fmt.Printf("Recebido de %s: %d\n", conn.LocalAddr().String(), n)
-	// write data to response
-	conn.Write([]byte(strconv.Itoa(fibo(n))))
-
-	// close conn
-	conn.Close()
+	conn.Close() // Close the connection after handling the request
 }
 
 func fibo(n int) int {
