b161336f2a6053339702d7363babf95b52a6c852
Zenio Angelo <72936093+Zenio2500@users.noreply.github.com>
2023-08-26 20:30:16 -0300

    Update Consumer.go

--- RabbitMQ/Consumer/Consumer.go
+++ RabbitMQ/Consumer/Consumer.go
@@ -1,58 +1,98 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 
-	"github.com/streadway/amqp"
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
-func main() {
-	conn, err := amqp.Dial("amqp://guest:guest@172.17.0.2:5672/")
+// configurations
+const RequestQueue = "request_queue"
+const ResponseQueue = "response_queue"
+
+type Request struct {
+	Num int
+}
+
+func failOnError(err error, msg string) {
 	if err != nil {
-		log.Fatalf("Failed to connect to RabbitMQ: %v", err)
+		log.Panicf("%s: %s", msg, err)
 	}
+}
+
+func fibo(n int) int {
+	ans := 1
+	prev := 0
+	for i := 1; i < n; i++ {
+		temp := ans
+		ans = ans + prev
+		prev = temp
+	}
+	return ans
+}
+
+func main() {
+	//connect to broker
+	conn, err := amqp.Dial("amqp://guest:guest@172.17.0.3:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
 
+	//create channel
 	ch, err := conn.Channel()
-	if err != nil {
-		log.Fatalf("Failed to open a channel: %v", err)
-	}
+	failOnError(err, "Failed to open a channel")
 	defer ch.Close()
 
-	queue, err := ch.QueueDeclare(
-		"hello",
-		false,
-		false,
-		false,
-		false,
-		nil,
+	//create request queue
+	q, err := ch.QueueDeclare(
+		RequestQueue, //routing key(queue's name)
+		false,        //durable
+		false,        //autodelete
+		false,        //exclusive
+		false,        //nowait
+		nil,          //args
 	)
-	if err != nil {
-		log.Fatalf("Failed to declare a queue: %v", err)
-	}
+	failOnError(err, "Failed to create request queue(broker's queue)")
 
+	//create request queue's consumer
 	msgs, err := ch.Consume(
-		queue.Name,
-		"",
-		true,
-		false,
-		false,
-		false,
-		nil,
+		q.Name, //routing key(queue's name)
+		"",     //consumer
+		true,   //autoACK
+		false,  //exclusive
+		false,  //noLocal
+		false,  //nowait
+		nil,    //args
 	)
-	if err != nil {
-		log.Fatalf("Failed to register a consumer: %v", err)
+	failOnError(err, "Failed to create response queue's consumer(broker's consumer)")
+
+	fmt.Println("Consumer is ready!")
+
+	//receive and process the messages
+	for d := range msgs {
+
+		//receive request
+		msg := Request{}
+		err := json.Unmarshal(d.Body, &msg)
+		failOnError(err, "Failed to deserialize the message")
+
+		//process request
+		replymsgBytes, err := json.Marshal(fibo(msg.Num))
+		//fmt.Println(fibo(msg.Num), msg.Num)
+		failOnError(err, "Failed to serialize the message")
+
+		//send(publish) response
+		err = ch.Publish(
+			"",        // exchange
+			d.ReplyTo, // routing key
+			false,     // mandatory
+			false,     // immediate
+			amqp.Publishing{
+				ContentType:   "text/plain",
+				CorrelationId: d.CorrelationId,
+				Body:          replymsgBytes,
+			})
+		failOnError(err, "Failed to send the message to the broker")
 	}
-
-	forever := make(chan bool)
-
-	go func() {
-		for d := range msgs {
-			fmt.Printf("Received a message: %s\n", d.Body)
-		}
-	}()
-
-	fmt.Println("Waiting for messages. To exit press CTRL+C")
-	<-forever
 }
